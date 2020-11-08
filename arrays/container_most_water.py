d3b628c7f49dcf9403e6602824d6d3f3ade05d73
Tim Gibson <tgibson5463@gmail.com>
2020-11-09 09:04:30 +1100

    container_most_water

---
+++ arrays/container_most_water.py
@@ -0,0 +1,30 @@
+"""Given n non-negative integers a1, a2, ..., an, where each represents a
+point at coordinate (i, ai), n vertical lines are drawn such that the two
+endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+with x-axis forms a container, such that the container contains the most water.
+Note: You may not slant the container and n is at least 2.
+Example:
+    Input: [1,8,6,2,5,4,8,3,7]
+    Output: 49
+"""
+from typing import List
+
+def max_area(self, height: List[int]) -> int:
+    """
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+    i = 0
+    j = len(height) - 1
+    water = float("-inf")
+
+    while i < j:
+        width = j - i
+        water = max(water, width * min(height[i], height[j]))
+
+        if height[i] > height[j]:
+            j -= 1
+        else:
+            i += 1
+
+    return water
